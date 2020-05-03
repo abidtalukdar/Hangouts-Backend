d5b68aaf805bf4fc492b24082af70647a875485d
erocknine <echeungsk@gmail.com>
2020-05-03 10:12:49 -0400

    added seed data, and controller actions

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,2 +1,8 @@
 class MembershipsController < ApplicationController
+
+  def index
+    memberships = Membership.all
+    render json: memberships
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.create(name: params[:name], first_name: params[:first_name], last_name: params[:last_name], img: params[:img], current_address: params[:current_address], default_address: params[:default_address]])
+    render json: user
+  end
+
 end
