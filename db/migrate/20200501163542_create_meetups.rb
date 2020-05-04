63ba9fe66eee8c49aa72002d6d0a044b9ccb0b4a
abidtalukdar <abiditalukdar@gmail.com>
2020-05-03 20:37:43 -0400

    updated meetups schema

--- db/migrate/20200501163542_create_meetups.rb
+++ db/migrate/20200501163542_create_meetups.rb
@@ -2,6 +2,7 @@ class CreateMeetups < ActiveRecord::Migration[6.0]
   def change
     create_table :meetups do |t|
       t.string :location
+      t.string :location_name
       t.date :date
       t.time :time
       t.string :image
