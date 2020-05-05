0b4288819d7351fbd6df7475d21c8afc0b39c779
96StanleyLee <96stanleylee@gmail.com>
2020-05-05 14:58:33 -0400

    changed users and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :meetups
 
-  get '/friends', to: 'users#friends'
+  get '/friends/:id', to: 'users#friends'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
